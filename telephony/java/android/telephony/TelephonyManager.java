7eb20d6efecc8d0e75e1c332cf5430c5a6d89c7d
Tobias Thierer <tobiast@google.com>
2016-04-21 17:51:41 +0100

    Avoid instantiations of Boolean that are immediately unboxed.
    Replaced Boolean.valueOf() -> parseBoolean() across frameworks/base
    (not including Am.java, which I'm leaving for a separate change).
    
    Also fixed a couple of other valueOf()s in SyncStorageEngine.
    
    Bug: 28289401
    Change-Id: I2c6fb39a465885e7e625ed8894b1b9f8a30da5bc

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -4641,7 +4641,7 @@ public class TelephonyManager {
      */
     public boolean getSmsReceiveCapableForPhone(int phoneId, boolean defaultValue) {
         if (SubscriptionManager.isValidPhoneId(phoneId)) {
-            return Boolean.valueOf(TelephonyManager.getTelephonyProperty(phoneId,
+            return Boolean.parseBoolean(TelephonyManager.getTelephonyProperty(phoneId,
                     TelephonyProperties.PROPERTY_SMS_RECEIVE, String.valueOf(defaultValue)));
         }
 
@@ -4672,7 +4672,7 @@ public class TelephonyManager {
      */
     public boolean getSmsSendCapableForPhone(int phoneId, boolean defaultValue) {
         if (SubscriptionManager.isValidPhoneId(phoneId)) {
-            return Boolean.valueOf(TelephonyManager.getTelephonyProperty(phoneId,
+            return Boolean.parseBoolean(TelephonyManager.getTelephonyProperty(phoneId,
                     TelephonyProperties.PROPERTY_SMS_SEND, String.valueOf(defaultValue)));
         }
 
