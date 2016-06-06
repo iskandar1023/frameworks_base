db2421f1ba7bd3582eb6bb06190e447e45071e5d
Jorge Martín <angel.arasthel@gmail.com>
2016-06-06 14:21:54 -0400

    Add ability to use 2 main tiles or disable for 3 normal tiles PureNexus edits: *Forward ported from lollipop *Also fixed dual tiles in stock quicksettings tuner
    previously having show main tiles on showed 3 tiles on the top row in quick settings tuner and didnt reflect what was actually shown in the quick settings panel this was resolved by removing main tiles from the observer, also added a call to refresh all tiles on drop and on tiles changed as without these the top main tiles would loose their main tiles look in the tuner after a tile was changed, also remove all references to supportsDualTargets

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6157,6 +6157,13 @@ public final class Settings {
         public static final String DEVICE_HOSTNAME = "device_hostname";
 
         /**
+         * Use "main" tiles on the first row of the quick settings panel
+         * 0 = no, 1 = yes
+         * @hide
+         */
+        public static final String QS_USE_MAIN_TILES = "sysui_qs_main_tiles";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
